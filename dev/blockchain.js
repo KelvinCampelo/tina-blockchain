65597dcf1cdf37ed6217db367fd026e37b1c7f69
Kelvin Campelo Alves de Sousa <kelvin.campelo@ifpi.edu.br>
2019-01-11 15:21:40 -0300

    add the method to hash our blocks

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,9 +1,15 @@
+const sha256 = require('sha256');
+
 function TinaBlockchain() {
   this.chain = [];
   this.pendingTransactions = [];
 }
 
-TinaBlockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+TinaBlockchain.prototype.createNewBlock = function(
+  nonce,
+  previousBlockHash,
+  hash
+) {
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date.now(),
@@ -20,19 +26,41 @@ TinaBlockchain.prototype.createNewBlock = function(nonce, previousBlockHash, has
 };
 
 TinaBlockchain.prototype.getLastBlock = function() {
-    return this.chain[this.chain.length - 1]
-}
+  return this.chain[this.chain.length - 1];
+};
 
-TinaBlockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
-    const newTransaction = {
-        amount,
-        sender,
-        recipient
-    }
+TinaBlockchain.prototype.createNewTransaction = function(
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount,
+    sender,
+    recipient
+  };
+
+  this.pendingTransactions.push(newTransaction);
+
+  return this.getLastBlock()['index'] + 1;
+};
+
+TinaBlockchain.prototype.hashBlock = function(
+  previousBlockHash,
+  currentBlockData,
+  nonce
+) {
+  const dataAsString = `${previousBlockHash}${nonce.toString()}${JSON.stringify(
+    currentBlockData
+  )}`;
+
+  const hash = sha256(dataAsString);
+
+  return hash;
+};
+
+TinaBlockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
     
-    this.pendingTransactions.push(newTransaction)
-
-    return this.getLastBlock()['index'] + 1;
 }
 
 module.exports = TinaBlockchain;
