c3d3a83f8dd3ec721bba347597fd90b31a258420
Kelvin Campelo Alves de Sousa <kelvin.campelo@ifpi.edu.br>
2019-01-14 08:28:46 -0300

    created the consensus endpoint into node

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -184,6 +184,54 @@ app.post('/register-nodes-bulk', (req, res) => {
     res.json({ note: 'Bulk registration successful.' });
 });
 
+app.get('/consensus', (req, res) => {
+    const requestPromises = [];
+    tina.networkNodes.forEach(networkNodeUrl => {
+        const requestOptions = {
+            uri: networkNodeUrl + '/blockchain',
+            method: 'GET',
+            json: true,
+        };
+        requestPromises.push(rp(requestOptions));
+    });
+
+    Promise.all(requestPromises).then(blockchains => {
+        const currentChainLength = tina.chain.length;
+        let maxChainLength = currentChainLength;
+        let newLongestChain = null;
+        let newPendingTransactions = null;
+
+        blockchains.forEach(blockchain => {
+            if (
+                blockchain.chain.length > maxChainLength &&
+                tina.chainIsValid(blockchain.chain)
+            ) {
+                maxChainLength = blockchain.chain.length;
+                newLongestChain = blockchain.chain;
+                newPendingTransactions = blockchain.pendingTransactions;
+            }
+        });
+
+        if (
+            !newLongestChain ||
+            (newLongestChain && !tina.chainIsValid(newLongestChain))
+        ) {
+            res.json({
+                note: 'Current chain has not been replaced.',
+                chain: tina.chain,
+            });
+        } else {
+            tina.chain = newLongestChain;
+            tina.pendingTransactions = newPendingTransactions;
+
+            res.json({
+                note: 'Current chain has been replaced.',
+                chain: tina.chain,
+            });
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
