d22f33c204106a9d140ac7d8d7b80a3b817e4ed5
Kelvin Campelo Alves de Sousa <kelvin.campelo@ifpi.edu.br>
2019-01-14 16:16:24 -0300

    created getBlock, getTransaction and get AddressData methods

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -136,4 +136,49 @@ TinaBlockchain.prototype.chainIsValid = function(blockchain) {
     return validChain;
 };
 
+TinaBlockchain.prototype.getBlock = function (blockHash) {
+    return this.chain.find(block => block.hash === blockHash)
+}
+
+TinaBlockchain.prototype.getTransaction = function(transactionId) {
+	let correctTransaction = null;
+	let correctBlock = null;
+
+	this.chain.forEach(block => {
+		block.transactions.forEach(transaction => {
+			if (transaction.transactionId === transactionId) {
+				correctTransaction = transaction;
+				correctBlock = block;
+			};
+		});
+	});
+
+	return {
+		transaction: correctTransaction,
+		block: correctBlock
+	};
+};
+
+TinaBlockchain.prototype.getAddressData = function(address) {
+	const addressTransactions = [];
+	this.chain.forEach(block => {
+		block.transactions.forEach(transaction => {
+			if(transaction.sender === address || transaction.recipient === address) {
+				addressTransactions.push(transaction);
+			};
+		});
+	});
+
+	let balance = 0;
+	addressTransactions.forEach(transaction => {
+		if (transaction.recipient === address) balance += transaction.amount;
+		else if (transaction.sender === address) balance -= transaction.amount;
+	});
+
+	return {
+		addressTransactions: addressTransactions,
+		addressBalance: balance
+	};
+};
+
 module.exports = TinaBlockchain;
